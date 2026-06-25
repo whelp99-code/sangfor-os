e6025df212bfe3fa5a1896d238c270d8f8da0c44
whelp99-code <whelp99@gmail.com>
2026-06-25 15:45:26 +0900

    fix: 패키지 구조 정리 + @sangfor 스코프 통일

--- packages/db_base/prisma/migrations/20260526120000_phase13_source_entity/migration.sql
+++
@@ -1,6 +0,0 @@
--- Phase 13 Sprint 1: optional portal entity linkage on command runs (additive)
-ALTER TABLE "command_runs" ADD COLUMN "source_entity_type" TEXT;
-ALTER TABLE "command_runs" ADD COLUMN "source_entity_id" TEXT;
-
-CREATE INDEX "command_runs_source_entity_type_source_entity_id_idx"
-  ON "command_runs" ("source_entity_type", "source_entity_id");
